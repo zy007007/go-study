7850e37c6cf497c66794f99dab457c375fc581d2
zy <806897446@qq.com>
2020-03-26 22:10:35 +0800

    20200326 code practice

--- addBinary.go
+++ addBinary.go
@@ -16,6 +16,14 @@ package main
 //个人思路
 //转换为10进制，相加后，再转换为2进制。还能顺便写两个2,10进制转换的函数，万一以后还能用呢
 
+//测试用例，下面这个出错
+// a "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101"
+// b "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011"
+// 私以为是math.Pow(2) 的问题
+
+// 看评论和题解
+// 大风向是把短的先补齐，具体实现待看
+
 import (
 	"fmt"
 	"math"
@@ -23,6 +31,13 @@ import (
 )
 
 func addBinary(a string, b string) string {
+
+	if a == "0" {
+		return b
+	}
+	if b == "0" {
+		return a
+	}
 
 	A := twoToTen(a)
 	B := twoToTen(b)
@@ -40,8 +55,8 @@ func twoToTen(n string) int {
 			tmp := math.Pow(2, float64(k))
 
 			sum += int(tmp)
-			k -= 1
 		}
+		k -= 1
 	}
 	return sum
 }
@@ -76,3 +91,5 @@ func main() {
 	fmt.Printf("输出:")
 	fmt.Println(addBinary(a, b))
 }
+
+//收获：倒也复习了二十进制间的转换
