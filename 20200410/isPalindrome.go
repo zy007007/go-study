a98d7b383559f0f72ce97d44705d05002920efe5
zy <806897446@qq.com>
2020-04-10 18:30:53 +0800

    20200410 code practice

---
+++ 20200410/isPalindrome.go
@@ -0,0 +1,21 @@
+package main
+
+//请判断一个链表是否为回文链表。
+//
+//示例 1:
+//
+//输入: 1->2
+//输出: false
+//示例 2:
+//
+//输入: 1->2->2->1
+//输出: true
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func isPalindrome(head *ListNode) bool {
+
+}
