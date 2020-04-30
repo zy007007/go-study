ce05834806bbb98470bec39e98de1da0d72f922f
zy <806897446@qq.com>
2020-04-30 11:37:20 +0800

    20200430 code practice

---
+++ 20200430/countSegments.go
@@ -0,0 +1,28 @@
+package main
+
+//统计字符串中的单词个数，这里的单词指的是连续的不是空格的字符。
+//
+//请注意，你可以假定字符串里不包括任何不可打印的字符。
+//
+//示例:
+//
+//输入: "Hello, my name is John"
+//输出: 5
+//解释: 这里的单词是指连续的不是空格的字符，所以 "Hello," 算作 1 个单词。
+
+// 个人思路
+// 直接用库函数吧
+
+import (
+	"fmt"
+)
+
+func countSegments(s string) int {
+	return len(strings.Fileds(s))
+}
+
+func main() {
+	fmt.Println("vim-go")
+}
+
+// 收获：leetcode　是把库函数给禁止了吗
