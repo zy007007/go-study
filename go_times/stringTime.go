a2861d091bc8ef39961cec8d8378f3fd438af4dc
yi zhao <806897446@qq.com>
2020-07-19 10:34:23 +0800

    strings time to Time

--- go_times/stringTime.go
+++ go_times/stringTime.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"time"
+	"strings"
 )
 
 // 如果结构体定义为 time.Time，在传入string的时间时，可用下面的函数转换为go时间格式
@@ -19,7 +20,15 @@ func ConverTime(ts string) time.Time {
 	return zeroTime
 }
 
+
+func ConverShortTime(ts string) string {
+	tsArray := strings.Split(ts, " ")
+	newts := strings.Join([]string{tsArray[0], "T", tsArray[1], "+08:00"}, "")
+	return newts
+}
+
+
 func main() {
-	t := "2020-07-07T17:07:07+08:00"
-	fmt.Println(ConverTime(t))
+	t := "2020-07-07 17:07:07"
+	fmt.Println(ConverTime(ConverShortTime(t)))
 }
